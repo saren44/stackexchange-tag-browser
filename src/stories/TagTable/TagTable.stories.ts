01dea0fc7be6084ebd33066ed2f27cf9a3c7ac84
saren44 <kuba.sarna741@gmail.com>
2024-04-04 15:28:39 +0200

    Add icons to sb, remove comments

--- src/stories/TagTable/TagTable.stories.ts
+++ src/stories/TagTable/TagTable.stories.ts
@@ -2,26 +2,20 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { TagTable } from '../../components/TagsTable/TagsTableNew';
 import { mockTagData } from '../../components/TagsTable/mockData';
 import { themeWrap } from '../themeWrapper';
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+
 
 
 const meta = {
   title: 'Example/Tag Table',
   component: TagTable,
   parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
     layout: 'padded',
   },
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
-  tags: ['autodocs'],
-  // More on argTypes: https://storybook.js.org/docs/api/argtypes
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
 } satisfies Meta<typeof TagTable>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Light: Story = {
 	decorators: [
 		(Story) => themeWrap(Story, true)
